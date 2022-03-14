f6583fc6e94a9957cefe0936ce2770ee1e005805
HYUCK7 <iqeq12191219@gmail.com>
2022-03-14 18:04:44 +0900

    2022-03-14 18:04 7

--- Algorithm/quiz20.py
+++ Algorithm/quiz20.py
@@ -1,6 +1,53 @@
 import random
+import urllib
+from urllib.request import urlopen
+import pandas as pd
+from bs4 import BeautifulSoup
+
+from Algorithm.domains import my100, members, myRandom
+
+
 class Quiz20:
-    def quiz20list(self) -> str: return None
+    def quiz20list(self) :
+        list1 = [1, 2, 3, 4]
+        print(list1, type(list1))  # [1,2,3,4], type: list
+        print(list1[0], list1[-1], list1[-2], list1[1:3])  # 1, 4, 3, [2,3]
+
+        list2 = ['math', 'english']
+        print(list2[0])  # ['math']
+        print(list2[0][1])  # [a]
+
+        list3 = [1, '2', [1, 2, 3]]
+        print(list3)  # [1,2,[1,2,3]]
+
+        list4 = [1, 2, 3]
+        list5 = [4, 5]
+        print(list4 + list5)  # [1,2,3,4,5]
+
+        print(2 * list4)  # [1,2,3,1,2,3]
+        list4.append(list5)
+        print(list4)  # [1,2,3,4,5]
+
+        list4[-2:] = []
+        print(list4)  # [1,2,3]
+
+        a = [1, 2]
+        b = [0, 5]
+        c = [a, b]
+        print(c)  # [[1,2],[0,5]]
+        print(c[0][1])  # [2]
+
+        c[0][1] = 10
+        print(c)  # [[1,10],[0,5]]
+
+        a = range(10)
+        print(a)  # range(0,10)
+        print(sum(a))  # 45
+        print(sorted(a))  # [0,1,2,3,4,5,6,7,8,9]
+
+        b = [2, 10, 0, -2]
+        print(sorted(b))  # -2, 0, 2, 10
+        print(b.index(0), len(b))
 
     def quiz21tuple(self) -> str: return None
 
@@ -8,14 +55,127 @@ class Quiz20:
 
     def quiz23listcom(self) -> str: return None
 
-    def quiz24zip(self) -> str: return None
+    def quiz24zip(self) -> {}:
+        '''a = [i for i in self.abc(soup, 'p', 'class', 'artist')]
+                       a = [i for i in self.abc(soup, 'p', 'class', 'title')]
+                       a = self.abc(soup, 'p', 'class', 'artist')
+                       b = self.abc(soup, 'p', 'class', 'title')
+                       print(a, b)'''
 
-    def quiz25dictcom(self) -> str: return None
+        url = 'https://music.bugs.co.kr/chart/track/realtime/total'
+        html_doc = urlopen(url)
+        soup = BeautifulSoup(html_doc.read(), 'lxml')  # html.parser vs lxml
+        ls1 = self.find_music(soup, 'title')
+        ls2 = self.find_music(soup, 'artist')
+        # self.dict1(ls1, ls2)
+        # self.dict2(ls1, ls2)
+        '''dict = {}
+        for i, j in zip(ls1, ls2):
+            dict[i] = j'''
+        dt = {i: j for i, j in zip(ls1, ls2)}
+        d1 = dict(zip(ls1, ls2))
+        l = [i + j for i, j in zip(ls1, ls2)]
+        l2 = list[ls1, ls2]
+        print(d1)
+        return dt
+
+    @staticmethod
+    def dict2(ls1, ls2) -> None:
+        dict = {}
+        for i, j in enumerate(ls1):
+            dict[j] = ls2[i]
+        print(dict)
+
+    @staticmethod
+    def dict1(ls1, ls2) -> None:
+        dict = {}
+        for i in range(0, len(ls1)):
+            dict[ls1[i]] = ls2[i]
+        print(dict)
+
+    def find_rang(self, soup) -> None:
+        for i, j in enumerate(['artist', 'title']):
+            for i, j in self.find_music(soup, j):
+                print(f'{i}위 : {j}')
+            print('*' * 100)
+
+    @staticmethod
+    def print_music_list(soup) -> None:
+        # print(soup.prettify())
+        artists = soup.find_all('p', {"class": "artist"})
+        # print(type(artists)) # <class 'b24.element.ResultSet'>
+        artists = ([i.get_text() for i in artists])
+        # print(type(artists))
+        # print(''.join(i for i in artists))
+        title = soup.find_all('p', {"class": "title"})
+        title = ([j.get_text() for j in title])
+        print(''.join(j for j in title))
+
+    @staticmethod
+    def find_music(soup, cls_name) -> []:
+        ls = soup.find_all('p', {'class': cls_name})
+        return [j.get_text() for j in ls]
+        # print(''.join(j for j in a))
+
+    def quiz25dictcom(self) -> str:
+        # memberlist()[myRandom(0,23)] 이것이 한 명인데 5명 추출
+        # scores는 0~100점 사이 랜덤
+        students = [members()[myRandom(0, 23)] for i in range(5)]
+        c = set(students)
+        while len(c) != 5:
+            c.add([members()[myRandom(0, 23)]])
+        students = list(c)
+        scores = [str(my100()) for i in range(5)]
+        # dict = {}
+        # for i, j in zip(students, scores):
+        #    dict[i] = j
+        a = {i: j for i, j in zip(students, scores)}
+        print(a)
+
+        return None
 
     def quiz26map(self) -> str: return None
 
-    def quiz27(self) -> str: return None
+    def quiz27melon(self) -> str:
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        url = 'https://www.melon.com/chart/index.htm?dayTime=2022030816'
+        req = urllib.request.Request(url, headers=headers)
+        soup = BeautifulSoup(urlopen(req).read(), 'lxml')
 
-    def quiz28(self) -> str: return None
+        artists = soup.find_all('div', {'class': 'ellipsis rank01'})
+        artists = [i.get_text() for i in artists]
+        artists = [i[1:-1] for i in artists]
+        # print(''.join([i for i in artists]))
+        print(''.join([i for i in artists]))
 
-    def quiz29(self) -> str: return None
+        print('-' * 30)
+
+        musics = soup.find_all('div', {'class': 'ellipsis rank02'})
+        musics = [i.get_text() for i in musics]
+        print(''.join([i for i in musics]))
+        return None
+
+    def quiz28dataframe(self) -> None:
+        dict = self.quiz24zip()
+        df = pd.DataFrame.from_dict(dict, orient='index')
+        print(df)
+        df.to_csv('./save/bugs.csv', sep=',', na_rep='NaN')
+        return None
+
+    def quiz29_pandas_df(self) -> object:
+        """
+                (1)data = {'a': [1, 2], 'b': [3, 4], 'c': [5, 6]}
+                data = {i}
+                print(data)
+                data = pd.DataFrame(data, index=['1', '2'])
+                print(data)
+                (2)d2 = {"1":[1,3,5], "2":[2,4,6]}
+                df2 = pd.DataFrame.from_dict(d2, orient= 'index', columns=['a','b','c'])
+                """
+        a = []
+        b = []
+        c = [chr(i) for i in range(97, 100)]  # ['a', 'b', 'c']
+        [a.append(i) if i % 2 == 0 else b.append(i) for i in range(1, 7)]
+        dict = {'1': b, '2': a}
+        df3 = pd.DataFrame.from_dict(dict, orient='index', columns=c)
+        print(df3)
--- Algorithm/quiz30.py
+++ Algorithm/quiz30.py
@@ -1,53 +1,173 @@
+import random
+import string
+
+import numpy as np
+import pandas as pd
+from icecream import ic
+from titanic.models import Model
+
+from Algorithm.domains import myRandom, members
+
+
 class Quiz30:
-    def quiz30(self) -> None:
-        list1 = [1,2,3,4]
-        print(list1, type(list1)) #[1,2,3,4], type: list
-        print(list1[0], list1[-1], list1[-2], list1[1:3]) # 1, 4, 3, [2,3]
+    def quiz30_df_4_by_3(self):
+        # (1)
+        a = []
+        b = []
+        c = []
+        d = []
+        col = [chr(i) for i in range(32, 35)]  # ['A','B','C']
+        [a.append(i) if i < 4 else b.append(i) for i in range(1, 7)]
+        [c.append(i) if i < 10 else d.append(i) for i in range(7, 13)]
+        dict = {'1': a, '2': b, '3': c, '4': d}
+        df = pd.DataFrame.from_dict(dict, orient='index', columns=col)
+        ic(df)
 
-        list2 = ['math', 'english']
-        print(list2[0]) # ['math']
-        print(list2[0][1]) # [a]
+        # (2)
+        e = [[j * 3 + i for i in range(1, 4)] for j in range(4)]
+        df1 = pd.DataFrame(e, index=range(1, 5), columns=col)
+        ic(df1)
+        # j = [1,2,3] * 3 + 1
 
-        list3 = [1,'2',[1,2,3]]
-        print(list3) # [1,2,[1,2,3]]
+        # 수업
+        l1 = [range(1, 4)]
+        l2 = [range(1, 4)]
+        l3 = [range(1, 4)]
+        l4 = [range(1, 4)]
 
-        list4 = [1,2,3]
-        list5 = [4,5]
-        print(list4 + list5) # [1,2,3,4,5]
+    def quiz31_rand_2_by_3(self):
+        c = [i for i in range(0, 3)]  # 0 , 1,  2
+        # (1)
+        # myRandom()
+        # [a.append(myRandom(10,100)) for i in range(3)]
+        # [b.append(myRandom(10,100)) for i in range(3)]
 
-        print(2*list4) # [1,2,3,1,2,3]
-        list4.append(list5)
-        print(list4) # [1,2,3,4,5]
+        # [a.append(myRandom(10, 100)) if i < 3 else b.append(myRandom(10, 100)) for i in range(6)]
+        # dict = {'0': a, '1': b}
+        # df = pd.DataFrame.from_dict(dict, orient='index', columns=c)
+        # ic(df)
 
-        list4[-2:] = []
-        print(list4) # [1,2,3]
+        # (2)
+        e = [[myRandom(10, 100) for i in range(3)] for j in range(2)]
+        df1 = pd.DataFrame(e, index=range(0, 2), columns=c)
+        ic(df1)
 
-        a=[1,2]
-        b=[0,5]
-        c= [a,b]
-        print(c) # [[1,2],[0,5]]
-        print(c[0][1]) # [2]
+    @staticmethod
+    def id(chr_size) -> str: return ''.join([random.choice(string.ascii_letters) for i in range(chr_size)])
 
-        c[0][1]=10
-        print (c) # [[1,10],[0,5]]
+    def quiz32_df_grade(self):
+        data1 = np.random.randint(0, 100, (10, 4))
+        col1 = ['국어', '영어', '수학', '사회']
+        idx = [self.id(chr_size=5) for i in range(10)]
+        df1 = pd.DataFrame(data1, index=idx, columns=col1)
 
-        a=range(10)
-        print(a) # range(0,10)
-        print(sum(a)) # 45
-        print(sorted(a)) # [0,1,2,3,4,5,6,7,8,9]
+        data2 = {i: j for i, j in zip(idx, data1)}
+        df2 = pd.DataFrame.from_dict(data2, orient='index', columns=col1)
 
-        b= [2,10,0,-2]
-        print(sorted(b)) # -2, 0, 2, 10
-        print(b.index(0),len(b))
+        ic(df1)
+        ic(df2)
+
+    def quiz33(self):
+        d = [dict(zip(['a', 'b', 'c', 'd'],
+                      np.random.randint(0, 100, 4))) for _ in range(3)]
+        df = pd.DataFrame(d)
+
+        df1 = self.createDF(keys=['a', 'b', 'c', 'd'],
+                            vals=np.random.randint(0, 100, 4),
+                            length=3)
+        # print(df1)
+
+        df2 = pd.DataFrame()
+
+        # myGrade.csv
+        '''subj = ['java', 'py', 'js', 'sql']
+        stud = members()
+        data = np.random.randint(0, 100, (24,4))
+        df500 = pd.DataFrame(data, index=stud, columns=subj)
+        ic(df500)'''
+
+        # df500.to_csv('./save/myGrade.csv', sep=',', na_rep='NaN')
+        # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
+        # grade.csv
+        model = Model()
+        grade_df = model.new_model('grade.csv')
+        ic(grade_df)
 
 
-    def quiz31(self) -> str: return None
 
-    def quiz32(self) -> str: return None
+    @staticmethod
+    def createDF(keys, vals, length):
+        return pd.DataFrame([dict(zip(keys, vals)) for _ in range(length)])
 
-    def quiz33(self) -> str: return None
+    def quiz34(self) -> str:
+        '''
+        df1.iloc[0]: a     0
+                 b    54
+                 c    74
+                 d    63
+                 Name: 0, dtype: int32
 
-    def quiz34(self) -> str: return None
+        ic(df1.iloc[[0]])
+
+        ic| df1.iloc[[0]]:     a   b   c   d
+                            0  86  64  13  46
+
+        ic(df1.iloc[[0, 1]])
+
+        ic| df1.iloc[[0, 1]]:   a   b  c   d
+                             0  22  92  7  29
+                             1  22  92  7  29
+
+        ic(df1.iloc[:3])
+
+                    a   b   c   d
+                  0  24  74  88  27
+                  1  24  74  88  27
+                  2  24  74  88  27
+
+        ic(df.iloc[[True, False, True]])
+
+        ic| df.iloc[[True, False, True]]:      a   b   c   d
+                                            0  14  77   5  43
+                                            2  30  93  14  96
+
+        ic(df.iloc[lambda x: x.index % 2 == 0])
+
+        ic| df.iloc[lambda x: x.index % 2 ==0]:     a   b   c   d
+                                        0  74  13  51  77
+                                        2  25  91  74  49
+        java -> , js => , py : (Lambda)
+
+        ic(df.iloc[0, 1])
+        ic| df.iloc[0,1]: 23
+
+        ic(df.iloc[[0, 2], [1, 3]])
+
+        ic| df.iloc[[0,2], [1,3]]:     b   d
+                                    0  63  98
+                                    2  62  31
+
+        ic(df.iloc[1:3, 0:3])
+
+        ic| df.iloc[1:3, 0:3]:     a   b   c
+                       1  66  43   3
+                       2  42   4  13
+
+        ic(df.iloc[:, [True, False, True, False]])
+
+        ic| df.iloc[:, [True,False,True,False]]:     a   c
+                                         0  24  40
+                                         1  54  61
+                                         2  47  64
+
+        ic(df.iloc[:, lambda df: [0, 2]])
+
+        ic| df.iloc[:, lambda df:[0,2]]:     a   c
+                                 0  59   0
+                                 1  47  58
+                                 2  16   7
+        return None
+        '''
 
     def quiz35(self) -> str: return None
 
